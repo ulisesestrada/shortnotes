c6890cdd4a0d8c49a14a85132152446a3d880c80
Ulises Estrada <ulises@estrada.com.mx>
2013-03-24 05:02:26 -0600

    Final

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<?php
+/*
+Template Name: Full Width Page
+*/
+?>
+	<!-- begin #main -->
+	<div id="main" class="fullwidth clearfix">
+			<h2><?php _e('Ooops! 404 error!', 'site5framework'); ?></h2>
+			<p><?php _e('Sorry! It seems that the page you are looking for is not here.', 'site5framework'); ?></p>
+	</div>
+	<!-- end #main -->
+<?php get_footer(); ?>
